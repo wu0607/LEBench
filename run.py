4787a22871dbd1eb07eac7eb861497fb41c1152d
pwu59 <pwu59@node0.pwu59.advosuwmadison-pg0.wisc.cloudlab.us>
2020-09-22 23:21:04 -0500

    CSV for 4 - disable-transparent-huge-pages

--- run.py
+++ run.py
@@ -156,7 +156,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         kern_version = sys.argv[1]
         print("[INFO] Configuring to boot into " + kern_version + ".")
-        generate_grub_file(WORKING_DIR + 'template/grub', kern_version)
+        generate_grub_file(WORKING_DIR + '/template/grub', kern_version)
         install_grub_file()
         sys.exit(0)
 
