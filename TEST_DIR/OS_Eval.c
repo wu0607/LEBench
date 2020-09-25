92ae07f9df1b8427e082fb40088ee2af108a9986
pwu59 <pwu59@c220g5-110513.wisc.cloudlab.us>
2020-09-25 13:57:20 -0500

    Add csv result for Ubuntu18.04

--- TEST_DIR/OS_Eval.c
+++ TEST_DIR/OS_Eval.c
@@ -603,7 +603,7 @@ void page_fault_test(struct timespec *diffTime) {
 	char a = *((char *)addr);
 	clock_gettime(CLOCK_MONOTONIC,&endTime);
 	
-	printf("read: %c\n", a);
+	// printf("read: %c\n", a);
 	syscall(SYS_munmap, addr, file_size);
         close(fd);
 	add_diff_to_sum(diffTime, endTime, startTime);
@@ -889,7 +889,7 @@ void context_switch_test(struct timespec *diffTime) {
 
 int msg_size = -1;
 int curr_iter_limit = -1;
-#define sock "/TEST_DIR/socket"
+#define sock "/tmp/socket"
 void send_test(struct timespec *timeArray, int iter, int *i) {
 	int retval;
 	int fds1[2], fds2[2];
@@ -902,7 +902,7 @@ void send_test(struct timespec *timeArray, int iter, int *i) {
 	struct sockaddr_un server_addr;
 	memset(&server_addr, 0, sizeof(struct sockaddr_un));
 	server_addr.sun_family = AF_UNIX;
-	strncpy(server_addr.sun_path, home, sizeof(server_addr.sun_path) - 1); 
+	// strncpy(server_addr.sun_path, home, sizeof(server_addr.sun_path) - 1); 
 	strncpy(server_addr.sun_path, sock, sizeof(server_addr.sun_path) - 1); 
 
 	int forkId = fork();
@@ -917,7 +917,7 @@ void send_test(struct timespec *timeArray, int iter, int *i) {
 		close(fds2[1]);
 
 		struct sockaddr_un client_addr;
-		socklen_t client_addr_len;
+		socklen_t client_addr_len = sizeof(struct sockaddr_un);
 	
 		int fd_server = socket(AF_UNIX, SOCK_STREAM, 0);
 		if (fd_server < 0) printf("[error] failed to open server socket.\n");
@@ -930,7 +930,7 @@ void send_test(struct timespec *timeArray, int iter, int *i) {
 
 		write(fds1[1], &w, 1);
 
-		int fd_connect = accept(fd_server, (struct sockaddr *) &client_addr, &client_addr_len);
+		int fd_connect = accept(fd_server, (struct sockaddr_un *) &client_addr, &client_addr_len);
 		if (DEBUG) printf("Connection accepted.\n");
 
 		read(fds2[0], &r, 1);
@@ -1017,7 +1017,7 @@ void recv_test(struct timespec *timeArray, int iter, int *i) {
 		close(fds2[1]);
 
 		struct sockaddr_un client_addr;
-		socklen_t client_addr_len;
+		socklen_t client_addr_len = sizeof(struct sockaddr_un);
 	
 		int fd_server = socket(AF_UNIX, SOCK_STREAM, 0);
 		if (fd_server < 0) printf("[error] failed to open server socket.\n");
@@ -1030,7 +1030,7 @@ void recv_test(struct timespec *timeArray, int iter, int *i) {
 
 		write(fds1[1], &w, 1);
 
-		int fd_connect = accept(fd_server, (struct sockaddr *) &client_addr, &client_addr_len);
+		int fd_connect = accept(fd_server, (struct sockaddr_un *) &client_addr, &client_addr_len);
 		if (DEBUG) printf("Connection accepted.\n");
 
 		read(fds2[0], &r, 1);
